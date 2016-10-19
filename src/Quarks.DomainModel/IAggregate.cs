0c81e3565511962af98a6a24c4e2c6c1da143906
victor.govorov <victorgovorov@mail.ru>
2016-10-19 22:28:30 +0500

    #4.x - introduced event sourced IAggregate

--- src/Quarks.DomainModel/IAggregate.cs
+++ src/Quarks.DomainModel/IAggregate.cs
@@ -1,5 +1,9 @@
-﻿namespace Quarks.DomainModel
+﻿using System.Collections.Generic;
+using Quarks.DomainModel.Events;
+
+namespace Quarks.DomainModel
 {
+
 	/// <summary>
 	/// Aggregate is a cluster of domain objects that can be treated as a single unit. 
 	/// </summary>
@@ -7,4 +11,27 @@
 	public interface IAggregate
 	{
 	}
+
+    /// <summary>
+    /// Aggregate is a cluster of domain objects that can be treated as a single unit. 
+    /// At any time, the aggregate contains the list of occurred events 
+    /// and can distinguish between those committed 
+    /// and those uncommitted that result in pending changes. 
+    /// </summary>
+    /// <typeparam name="TEvent">Type of events.</typeparam>
+    /// <see href="http://martinfowler.com/bliki/DDD_Aggregate.html"/>
+    /// <see href="https://msdn.microsoft.com/magazine/mt767692"/>
+    public interface IAggregate<TEvent> : IAggregate where TEvent : IEntityEvent
+    {
+        /// <summary>
+        /// Collection of events in the sequence they were applied to the aggregate.
+        /// </summary>
+        IEnumerable<TEvent> OccurredEvents { get; }
+        
+        /// <summary>
+        /// Applies the specified event to the aggregate.
+        /// </summary>
+        /// <param name="evnt">Event should be applied.</param>
+        void ApplyEvent(TEvent evnt);
+    }
 }
