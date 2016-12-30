7c7f5ff6440a370fd6d1b4d92dcfbd068a98b788
victor.govorov <victorgovorov@mail.ru>
2016-12-31 00:36:02 +0500

    add ValueObject

---
+++ src/Quarks.DomainModel/ValueObject.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Quarks.DomainModel
+{
+    /// <summary>
+	/// An object that contains attributes but has no conceptual identity. They should be treated as immutable.
+	/// </summary>
+	/// <see href="http://martinfowler.com/bliki/ValueObject.html"/>
+    public abstract class ValueObject<T> : IEquatable<T>, IValueObject
+        where T : ValueObject<T>
+    {
+        /// <summary>Determines whether the specified object is equal to the current object.</summary>
+        /// <returns>true if the specified object  is equal to the current object; otherwise, false.</returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((T)obj);
+        }
+
+        /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
+        /// <returns>true if the current object is equal to the <paramref name="other" /> parameter; otherwise, false.</returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public virtual bool Equals(T other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (other.GetType() != GetType()) return false;
+
+            IEnumerable<FieldInfo> fields = GetFields();
+
+            foreach (FieldInfo field in fields)
+            {
+                object value1 = field.GetValue(other);
+                object value2 = field.GetValue(this);
+
+                if (Equals(value1, value2) == false)
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>Serves as the default hash function. </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            IEnumerable<FieldInfo> fields = GetFields();
+
+            int hashCode = 17;
+
+            foreach (FieldInfo field in fields)
+            {
+                object value = field.GetValue(this);
+
+                if (value != null)
+                    hashCode = hashCode * 59 + value.GetHashCode();
+            }
+
+            return hashCode;
+        }
+
+        private IEnumerable<FieldInfo> GetFields()
+        {
+            Type type = GetType();
+
+            List<FieldInfo> fields = new List<FieldInfo>();
+
+            while (type != typeof(object))
+            {
+                fields.AddRange(type.GetTypeInfo().DeclaredFields);
+
+                type = type.GetTypeInfo().BaseType;
+            }
+
+            return fields;
+        }
+
+        public static bool operator ==(ValueObject<T> x, ValueObject<T> y)
+        {
+            return Equals(x, y);
+        }
+
+        public static bool operator !=(ValueObject<T> x, ValueObject<T> y)
+        {
+            return !(x == y);
+        }
+    }
+}
---
+++ tests/Quarks.DomainModel.Tests/ValueObjectTests.cs
@@ -0,0 +1,169 @@
+﻿using JetBrains.Annotations;
+using NUnit.Framework;
+
+namespace Quarks.DomainModel.Tests
+{
+    [TestFixture]
+    public class ValueObjectTests
+    {
+        [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+        private class Address : ValueObject<Address>
+        {
+            public Address(string address1, string city, string state)
+            {
+                Address1 = address1;
+                City = city;
+                State = state;
+            }
+
+            public string Address1 { get; }
+
+            public string City { get; }
+
+            public string State { get; }
+        }
+
+        private class ExpandedAddress : Address
+        {
+            public ExpandedAddress(string address1, string address2, string city, string state)
+                : base(address1, city, state)
+            {
+                Address2 = address2;
+            }
+
+            public string Address2 { get; }
+        }
+
+        [Test]
+        public void AddressEqualsWorksWithIdenticalAddresses()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address1", "Austin", "TX");
+
+            Assert.That(address.Equals(address2), Is.True);
+        }
+
+        [Test]
+        public void AddressEqualsWorksWithNonIdenticalAddresses()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address2", "Austin", "TX");
+
+            Assert.That(address.Equals(address2), Is.False);
+        }
+
+        [Test]
+        public void AddressEqualsWorksWithNulls()
+        {
+            Address address = new Address(null, "Austin", "TX");
+            Address address2 = new Address("Address2", "Austin", "TX");
+
+            Assert.That(address.Equals(address2), Is.False);
+        }
+
+        [Test]
+        public void AddressEqualsWorksWithNullsOnOtherObject()
+        {
+            Address address = new Address("Address2", "Austin", "TX");
+            Address address2 = new Address("Address2", null, "TX");
+
+            Assert.That(address.Equals(address2), Is.False);
+        }
+
+        [Test]
+        public void AddressEqualsIsReflexive()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+
+            Assert.That(address.Equals(address), Is.True);
+        }
+
+        [Test]
+        public void AddressEqualsIsSymmetric()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address2", "Austin", "TX");
+
+            Assert.That(address.Equals(address2), Is.False);
+            Assert.That(address2.Equals(address), Is.False);
+        }
+
+        [Test]
+        public void AddressEqualsIsTransitive()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address1", "Austin", "TX");
+            Address address3 = new Address("Address1", "Austin", "TX");
+
+            Assert.That(address.Equals(address2), Is.True);
+            Assert.That(address2.Equals(address3), Is.True);
+            Assert.That(address.Equals(address3), Is.True);
+        }
+
+        [Test]
+        public void AddressOperatorsWork()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address1", "Austin", "TX");
+            Address address3 = new Address("Address2", "Austin", "TX");
+
+            Assert.That(address == address2, Is.True);
+            Assert.That(address2 != address3, Is.True);
+        }
+
+        [Test]
+        public void DerivedTypesBehaveCorrectly()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            ExpandedAddress address2 = new ExpandedAddress("Address1", "Apt 123", "Austin", "TX");
+
+            Assert.That(address.Equals(address2), Is.False);
+            Assert.That(address == address2, Is.False);
+        }
+
+        [Test]
+        public void EqualValueObjectsHaveSameHashCode()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address1", "Austin", "TX");
+
+            Assert.That(address.GetHashCode() == address2.GetHashCode(), Is.True);
+        }
+
+        [Test]
+        public void TransposedValuesGiveDifferentHashCodes()
+        {
+            Address address = new Address(null, "Austin", "TX");
+            Address address2 = new Address("TX", "Austin", null);
+
+            Assert.That(address.GetHashCode() == address2.GetHashCode(), Is.False);
+        }
+
+        [Test]
+        public void UnequalValueObjectsHaveDifferentHashCodes()
+        {
+            Address address = new Address("Address1", "Austin", "TX");
+            Address address2 = new Address("Address2", "Austin", "TX");
+
+            Assert.That(address.GetHashCode() == address2.GetHashCode(), Is.False);
+        }
+
+        [Test]
+        public void TransposedValuesOfFieldNamesGivesDifferentHashCodes()
+        {
+            Address address = new Address("_city", null, null);
+            Address address2 = new Address(null, "_address1", null);
+
+            Assert.That(address.GetHashCode() == address2.GetHashCode(), Is.False);
+        }
+
+        [Test]
+        public void DerivedTypesHashCodesBehaveCorrectly()
+        {
+            ExpandedAddress address = new ExpandedAddress("Address99999", "Apt 123", "New Orleans", "LA");
+            ExpandedAddress address2 = new ExpandedAddress("Address1", "Apt 123", "Austin", "TX");
+
+            Assert.That(address.GetHashCode() == address2.GetHashCode(), Is.False);
+        }
+    }
+}
